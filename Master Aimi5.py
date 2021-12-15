190875749b88c14f3f2449a425dba27ed022605d
sadraakhtarshenas <96003970+sadraakhtarshenas@users.noreply.github.com>
2021-12-15 12:30:49 -0800

    Add files via upload

---
+++ Master Aimi5.py
@@ -0,0 +1,23 @@
+
+w = int(input("arz ra vared kon: "))
+h = int(input("tool ra vared kon: "))
+a = w * h
+p = (w + h) * a
+print("masahat:",a,"mohit:",p)
+
+
+
+r = int(input())
+a = r * r * 3.14
+p = r * r * 3.14
+print("masahat:", a, "mohit", p)
+
+
+
+import math
+
+r = int(input())
+a = r * r * math.pi
+p = r * r * math.pi
+print(math.pi)
+print("masahat:", a, "mohit", p)
